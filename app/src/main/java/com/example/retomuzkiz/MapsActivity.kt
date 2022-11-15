6c09149e17ec415ea86ae81268ae3a176f4b9769
Youssef <binwunderbar@gmail.com>
2022-11-16 00:04:48 +0100

    añadir drawer navigation

--- app/src/main/java/com/example/retomuzkiz/MapsActivity.kt
+++ app/src/main/java/com/example/retomuzkiz/MapsActivity.kt
@@ -1,12 +1,14 @@
 package com.example.retomuzkiz
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-
+import androidx.appcompat.widget.Toolbar
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -14,38 +16,35 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.retomuzkiz.databinding.ActivityMapsBinding
+import com.google.android.material.navigation.NavigationView
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback,NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     lateinit var drawer: DrawerLayout
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
-        drawer = binding.drawer
-        val drawerToggle = ActionBarDrawerToggle(this,drawer,R.string.open,R.string.close)
-        drawer.addDrawerListener(drawerToggle)
-        drawerToggle.syncState()
-
+        //__________________________________________________________________________________________
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        drawer = binding.drawerLayut
+        toggle = ActionBarDrawerToggle(this,drawer,toolbar,R.string.open,R.string.close)
+        drawer.addDrawerListener(toggle)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        val navigationView : NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+        //__________________________________________________________________________________________
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-    }
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            android.R.id.home -> {
-                drawer.openDrawer(GravityCompat.START)
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
     }
 
     /**
@@ -62,8 +61,37 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Add a marker in Sydney and move the camera
         //43.346497,-3.121751
-        val PobeñakoErmita = LatLng(43.346497, -3.121751)
-        mMap.addMarker(MarkerOptions().position(PobeñakoErmita).title("Pobeñako Ermita"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(PobeñakoErmita))
+        val pobenakoErmita = LatLng(43.346497, -3.121751)
+        mMap.addMarker(MarkerOptions().position(pobenakoErmita).title("Pobeñako Ermita"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(pobenakoErmita))
     }
+    //______________________________________________________________________________________________
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.m_home -> Toast.makeText(this," home", Toast.LENGTH_SHORT).show()
+            R.id.m_link -> Toast.makeText(this," Link", Toast.LENGTH_SHORT).show()
+            R.id.m_logout -> Toast.makeText(this," Logout", Toast.LENGTH_SHORT).show()
+            R.id.m_perfil -> Toast.makeText(this," Perfil", Toast.LENGTH_SHORT).show()
+        }
+        drawer.closeDrawer(GravityCompat.START)
+        return true
+    }
+    //______________________________________________________________________________________________
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        toggle.syncState()
+    }
+    //______________________________________________________________________________________________
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        toggle.onConfigurationChanged(newConfig)
+    }
+    //______________________________________________________________________________________________
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    //______________________________________________________________________________________________
 }
