cdd09f1c90bc5cad4cef067fcfa432f7ca318c9d
JonGonzalezPallares <73275978+JonGonzalezPallares@users.noreply.github.com>
2022-11-16 10:28:54 +0100

    Menu principal + cosas necesarias

--- app/src/main/java/com/example/retomuzkiz/MenuPrincipal.kt
+++ app/src/main/java/com/example/retomuzkiz/MenuPrincipal.kt
@@ -9,6 +9,8 @@ import com.example.retomuzkiz.databinding.ActivityMenuPrincipalBinding
 class MenuPrincipal : AppCompatActivity() {
 
     private lateinit var binding : ActivityMenuPrincipalBinding
+    private lateinit var lista: List<Jugador>
+    private lateinit var adaptador: JugadoresAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +20,26 @@ class MenuPrincipal : AppCompatActivity() {
         //Quitamos la barra superior para que se vea mejor
         this.supportActionBar!!.hide()
 
+        //Prueba para ver como queda el recycleview, quitarlo cuando tengamos jugadores
+        carga()
+
+        adaptador = JugadoresAdapter(lista)
+
+        binding.rvJugadores.adapter = adaptador
+
         binding.button.setOnClickListener {
             val intento = Intent(this, MapsActivity::class.java)
             startActivity(intento)
         }
+    }
+
+    fun carga() {
+        lista = listOf(
+            Jugador("Jugador 1", "29"),
+            Jugador("Jugador 2", "940"),
+            Jugador("Jugador 3", "94028"),
+            Jugador("Jugador 4", "2048"),
+            Jugador("Jugador 5", "299382"),
+        )
     }
 }
