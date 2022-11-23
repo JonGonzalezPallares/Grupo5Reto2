a5f35486b2cc3d08bd791852dc685ff4b68927df
JonGonzalezPallares <73275978+JonGonzalezPallares@users.noreply.github.com>
2022-11-23 12:22:27 +0100

    Empezado itsaslur

---
+++ app/src/main/java/com/example/retomuzkiz/itsaslurIbilbidea/ItsaslurJuego.kt
@@ -0,0 +1,22 @@
+package com.example.retomuzkiz.itsaslurIbilbidea
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.retomuzkiz.databinding.ActivityItsaslurJuegoBinding
+import com.example.retomuzkiz.databinding.ActivityPantallaEsperaBinding
+
+class ItsaslurJuego : AppCompatActivity() {
+
+    private lateinit var binding : ActivityItsaslurJuegoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityItsaslurJuegoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Para borrar la barra superior
+        this.supportActionBar!!.hide()
+
+
+    }
+}
