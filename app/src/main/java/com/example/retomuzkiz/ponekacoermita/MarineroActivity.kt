c63048da58247a7775efe4d719188ca70c8ae82b
daniel <dani30316diez@gmail.com>
2022-11-23 10:04:47 +0100

    ermita v0.1

---
+++ app/src/main/java/com/example/retomuzkiz/ponekacoermita/MarineroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.retomuzkiz.ponekacoermita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.retomuzkiz.R
+
+class MarineroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_marinero)
+    }
+}
