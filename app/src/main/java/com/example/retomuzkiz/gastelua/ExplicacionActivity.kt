b2fb0571dd3ad2d1f84e8e7b745cba430afe662c
daniel <dani30316diez@gmail.com>
2022-11-24 08:50:38 +0100

    gastelua v0.1

---
+++ app/src/main/java/com/example/retomuzkiz/gastelua/ExplicacionActivity.kt
@@ -0,0 +1,18 @@
+package com.example.retomuzkiz.gastelua
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.retomuzkiz.R
+import com.example.retomuzkiz.databinding.ActivityExplicacionBinding
+import com.example.retomuzkiz.databinding.ActivityMapsBinding
+import com.example.retomuzkiz.ponekacoermita.juegodemarActivity
+
+class ExplicacionActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityExplicacionBinding
+            override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explicacion)
+                binding = ActivityExplicacionBinding.inflate(layoutInflater)
+                setContentView(binding.root)
+    }
+}
