dbde81cad67db528fd937fbca06785634936dd45
JonGonzalezPallares <73275978+JonGonzalezPallares@users.noreply.github.com>
2022-12-22 13:26:31 +0100

    Activity del marinero

--- app/src/main/java/com/example/retomuzkiz/ponekakoermita/MarineroActivity.kt
+++ app/src/main/java/com/example/retomuzkiz/ponekakoermita/MarineroActivity.kt
@@ -3,6 +3,7 @@ package com.example.retomuzkiz.ponekakoermita
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.content.ContextCompat
 import com.example.retomuzkiz.R
 import com.example.retomuzkiz.databinding.ActivityMarineroBinding
@@ -21,6 +22,13 @@ class MarineroActivity : AppCompatActivity() {
         binding.btnsiguiente.setOnClickListener {
             val actividad = Intent(this, JuegodemarActivity::class.java)
             ContextCompat.startActivity(this, actividad, null)
+        }
+
+        binding.button2.setOnClickListener {
+            binding.txt1.visibility = View.GONE
+            binding.txt2.visibility = View.VISIBLE
+            binding.btnsiguiente.visibility = View.VISIBLE
+            it.visibility = View.GONE
         }
     }
 }
