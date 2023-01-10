045575effa46fb27ee57dc2236585860400e8568
JonGonzalezPallares <73275978+JonGonzalezPallares@users.noreply.github.com>
2023-01-10 13:21:15 +0100

    activity sobre nosotros

---
+++ app/src/main/java/com/example/retomuzkiz/Nosotros.kt
@@ -0,0 +1,11 @@
+package com.example.retomuzkiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Nosotros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nosotros)
+    }
+}
