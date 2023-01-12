c4c05d3f1ee89f17e8db45d0ac741efb183a0bd9
JonGonzalezPallares <73275978+JonGonzalezPallares@users.noreply.github.com>
2023-01-12 13:41:50 +0100

    cambios activity sobre nosotros

--- app/src/main/java/com/example/retomuzkiz/Nosotros.kt
+++ app/src/main/java/com/example/retomuzkiz/Nosotros.kt
@@ -1,14 +1,27 @@
 package com.example.retomuzkiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.retomuzkiz.databinding.ActivityMenuPrincipalBinding
+import com.example.retomuzkiz.databinding.ActivityNosotrosBinding
 
 class Nosotros : AppCompatActivity() {
+
+    private lateinit var binding : ActivityNosotrosBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_nosotros)
+        binding = ActivityNosotrosBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         //Para borrar la barra superior
         this.supportActionBar!!.hide()
+
+        binding.btnVolver.setOnClickListener {
+            val intento = Intent(this, MapsActivity::class.java)
+            this.finish()
+            startActivity(intento)
+        }
     }
 }
