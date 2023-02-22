a87a6fd124b59c2cea6c5325e6e3e013ee4f8e38
daniel <dani30316diez@gmail.com>
2023-02-22 09:12:36 +0100

    a

--- app/src/main/java/com/example/retomuzkiz/burdinola/main/SopaLetrasActivity.kt
+++ app/src/main/java/com/example/retomuzkiz/burdinola/main/SopaLetrasActivity.kt
@@ -1,12 +1,15 @@
 package com.example.retomuzkiz.burdinola.main
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.retomuzkiz.Laberinto.ActivityLaberinto
 import com.example.retomuzkiz.R
 import com.example.retomuzkiz.burdinola.game.GameFragment
 
 
 class SopaLetrasActivity : AppCompatActivity() {
+    lateinit var mediaPlay: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.juego_sopa)
@@ -17,5 +20,24 @@ class SopaLetrasActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_prencipal, exampleFragment)
             .commit()
+    }
+    override fun onPause() {
+
+        super.onPause()
+        mediaPlay!!.stop()
+        mediaPlay!!.release()
+        //musica("sanjuan",false,this)
+        if(ActivityLaberinto.cambio){
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mediaPlay = MediaPlayer.create(this,com.example.retomuzkiz.R.raw.nyan_cat)
+        mediaPlay.isLooping = true
+        mediaPlay.start()
+
+        //musica("sanjuan",true,this)
     }
 }
